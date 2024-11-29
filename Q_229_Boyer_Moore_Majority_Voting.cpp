80776386c539015ff3306c72bccd98e6d2b80e35
Shivansh Mittal <www.shivansh065@gmail.com>
2024-11-29 10:32:41 +0530

    added optimal for 229

--- Q_229_Boyer_Moore_Majority_Voting.cpp
+++ Q_229_Boyer_Moore_Majority_Voting.cpp
@@ -1,7 +1,4 @@
-#include <iostream>
-#include <vector>
-#include <unordered_map>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
 
 // Solution class
@@ -41,11 +38,51 @@ public:
         //     mpp[it]++;
         //     if (mpp[it] > nums.size() / 3 && find(vec.begin(), vec.end(), it) == vec.end())
         //         vec.push_back(it);
+        //     if(vec.size()==2)
+        //         break;
         // }
         // return vec;
 
-        // Optimal Approach - Boyer Moore Voting Algorithm(TC: O())
-        
+        // Optimal Approach - Boyer Moore Voting Algorithm(TC: O(n), SC: O(1))
+        int cnt1 = 0, cnt2 = 0;
+        int ele1 = INT_MIN, ele2 = INT_MIN;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(cnt1==0 && nums[i]!=ele2)
+            {
+                cnt1 = 1;
+                ele1 = nums[i];
+            }
+            else if(cnt2==0 && nums[i]!=ele1)
+            {
+                cnt2 = 1;
+                ele2 = nums[i];
+            }
+            else if(ele1 == nums[i])
+                cnt1++;
+            else if(ele2 == nums[i])
+                cnt2++;
+            else
+            {
+                cnt1--;
+                cnt2--;
+            }
+        }
+        vector<int> vec;
+        cnt1 = 0;
+        cnt2 = 0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(ele1==nums[i])
+                cnt1++;
+            if(ele2==nums[i])
+                cnt2++;
+        }
+        if(cnt1>(int)nums.size()/3)
+            vec.push_back(ele1);
+        if(cnt2>(int)nums.size()/3)
+            vec.push_back(ele2);
+        return vec;
     }
 };
 
