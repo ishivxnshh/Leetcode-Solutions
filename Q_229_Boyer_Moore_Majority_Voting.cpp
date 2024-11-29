5c81f1ae74379afc56145bf68775d32d37171e25
Shivansh Mittal <www.shivansh065@gmail.com>
2024-11-29 10:33:12 +0530

    formatted

--- Q_229_Boyer_Moore_Majority_Voting.cpp
+++ Q_229_Boyer_Moore_Majority_Voting.cpp
@@ -46,21 +46,21 @@ public:
         // Optimal Approach - Boyer Moore Voting Algorithm(TC: O(n), SC: O(1))
         int cnt1 = 0, cnt2 = 0;
         int ele1 = INT_MIN, ele2 = INT_MIN;
-        for(int i=0;i<nums.size();i++)
+        for (int i = 0; i < nums.size(); i++)
         {
-            if(cnt1==0 && nums[i]!=ele2)
+            if (cnt1 == 0 && nums[i] != ele2)
             {
                 cnt1 = 1;
                 ele1 = nums[i];
             }
-            else if(cnt2==0 && nums[i]!=ele1)
+            else if (cnt2 == 0 && nums[i] != ele1)
             {
                 cnt2 = 1;
                 ele2 = nums[i];
             }
-            else if(ele1 == nums[i])
+            else if (ele1 == nums[i])
                 cnt1++;
-            else if(ele2 == nums[i])
+            else if (ele2 == nums[i])
                 cnt2++;
             else
             {
@@ -71,16 +71,16 @@ public:
         vector<int> vec;
         cnt1 = 0;
         cnt2 = 0;
-        for(int i=0;i<nums.size();i++)
+        for (int i = 0; i < nums.size(); i++)
         {
-            if(ele1==nums[i])
+            if (ele1 == nums[i])
                 cnt1++;
-            if(ele2==nums[i])
+            if (ele2 == nums[i])
                 cnt2++;
         }
-        if(cnt1>(int)nums.size()/3)
+        if (cnt1 > (int)nums.size() / 3)
             vec.push_back(ele1);
-        if(cnt2>(int)nums.size()/3)
+        if (cnt2 > (int)nums.size() / 3)
             vec.push_back(ele2);
         return vec;
     }
