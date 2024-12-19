e2a2de676a2121560ff4477bc40a3967b63a9867
Shivansh Mittal <www.shivansh065@gmail.com>
2024-12-20 01:00:58 +0530

    added 15

--- Q_15.cpp
+++ Q_15.cpp
@@ -27,7 +27,63 @@ public:
         // }
         // return vec;
 
-        //Better Approach ()
+        //Better Approach (TC: O(n^2*logm), sc(WORST CASE):O(n+2*no. of unique triplets))
+        // vector<vector<int>> vec;
+        // set<vector<int>> st;
+        // for(int i=0;i<nums.size();i++)
+        // {
+        //     set<int> st1;
+        //     for(int j=i+1;j<nums.size();j++)
+        //     {
+        //         int third = -(nums[i] + nums[j]);
+        //         if(st1.find(third)!=st1.end())
+        //         {
+        //             vector<int> temp = {nums[i], nums[j], third};
+        //             sort(temp.begin(), temp.end());
+        //             st.insert(temp);
+        //         }
+        //         st1.insert(nums[j]);
+        //     }
+        // }
+        // for(auto it:st)
+        // {
+        //     vec.push_back(it);
+        // }
+        // return vec;
+
+        // Optimized Approach (TC: O(nlogn + n^2), sc(WORST CASE):O(no. of unique triplets))
+        vector<vector<int>> vec;
+        sort(nums.begin(), nums.end());
+        for(int i=0;i<nums.size()-2;i++)
+        {
+            if(i>0 && nums[i]==nums[i-1])
+                continue;
+            int j = i + 1;
+            int k = nums.size() - 1;
+            while(j<k)
+            {
+                int sum = nums[i]+nums[j]+nums[k];
+                if(sum<0)
+                {
+                    j++;
+                }
+                else if(sum>0)
+                {
+                    k--;
+                }
+                else
+                {
+                    vec.push_back({nums[i], nums[j], nums[k]});
+                    j++;
+                    k--;
+                    while(j<k && nums[j]==nums[j-1])
+                        j++;
+                    while(j<k && nums[k]==nums[k+1])
+                        k--;
+                }
+            }
+        }
+        return vec;
     }
 };
 
