8d03d15f86809831eaca19b516ce6d9fd134e956
Shivansh Mittal <www.shivansh065@gmail.com>
2025-01-02 14:15:40 +0530

    refactor merge function and improve code readability

--- Q_88.cpp
+++ Q_88.cpp
@@ -3,14 +3,40 @@
 
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+    void merge(vector<int> &nums1, int m, vector<int> &nums2, int n)
+    {
         
+        int i = m - 1;     // Pointer for the last valid element in nums1
+        int j = n - 1;     // Pointer for the last element in nums2
+        int k = m + n - 1; // Pointer for the last position in nums1
+
+        // Merge nums1 and nums2 starting from the back
+        while (i >= 0 && j >= 0)
+        {
+            if (nums1[i] > nums2[j])
+            {
+                nums1[k--] = nums1[i--]; // Place the larger element in the
+                                         // correct position
+            }
+            else
+            {
+                nums1[k--] = nums2[j--];
+            }
+        }
+
+        // Copy any remaining elements from nums2 into nums1
+        while (j >= 0)
+        {
+            nums1[k--] = nums2[j--];
+        }
     }
 };
 
-int main() {
+int main()
+{
     Solution solution;
 
     // Predefined test case
@@ -20,11 +46,13 @@ int main() {
     int n = 3;
 
     cout << "Before merge:\nNums1: ";
-    for (int num : nums1) {
+    for (int num : nums1)
+    {
         cout << num << " ";
     }
     cout << "\nNums2: ";
-    for (int num : nums2) {
+    for (int num : nums2)
+    {
         cout << num << " ";
     }
     cout << "\n";
@@ -34,7 +62,8 @@ int main() {
 
     // Output the merged array
     cout << "After merge:\nNums1: ";
-    for (int num : nums1) {
+    for (int num : nums1)
+    {
         cout << num << " ";
     }
     cout << "\n";
