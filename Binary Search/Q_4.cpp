ae526cfe2dfed57a0d095b5305cb03fe3d0e1479
Shivansh Mittal <www.shivansh065@gmail.com>
2025-06-07 00:01:08 +0530

    added

--- Binary Search/Q_4.cpp
+++ Binary Search/Q_4.cpp
@@ -40,55 +40,78 @@ public:
         //     return (static_cast<double>(middle1) + static_cast<double>(middle2)) / 2;
         // }
 
-        // Optimal Approach (tc: O(n1+n2)/2, sc: O(n1+n2)), Worst Case TC: O(n1+n2)
+        // Better Approach (tc: O(n1+n2)/2, sc: O(1)), Worst Case TC: O(n1+n2)
+        // int n1 = nums1.size();
+        // int n2 = nums2.size();
+        // int i = 0;
+        // int j = 0;
+        // int n = n1 + n2;
+        // int ind2 = n / 2;
+        // int ind1 = ind2 - 1;
+        // int ind1el = 0;
+        // int ind2el = 0;
+        // int count = 0;
+        // while (i < n1 && j < n2 && count <= ind2)
+        // {
+        //     if (nums1[i] < nums2[j])
+        //     {
+        //         if (count == ind1) ind1el = nums1[i];
+        //         if (count == ind2) ind2el = nums1[i];
+        //         i++;
+        //     }
+        //     else
+        //     {
+        //         if (count == ind1) ind1el = nums2[j];
+        //         if (count == ind2) ind2el = nums2[j];
+        //         j++;
+        //     }
+        //     count++;
+        // }
+        // while (i < n1 && count <= ind2)
+        // {
+        //     if (count == ind1) ind1el = nums1[i];
+        //     if (count == ind2) ind2el = nums1[i];
+        //     i++;
+        //     count++;
+        // }
+        // while (j < n2 && count <= ind2)
+        // {
+        //     if (count == ind1) ind1el = nums2[j];
+        //     if (count == ind2) ind2el = nums2[j];
+        //     j++;
+        //     count++;
+        // }
+        // if (n % 2 == 0)
+        //     return (double)(ind1el + ind2el) / 2.0;
+        // else
+        //     return (double)ind2el;
+
+        // Optimal Approach (tc: O(log(min(n1, n2))), sc: O(1))
         int n1 = nums1.size();
         int n2 = nums2.size();
-        int i = 0;
-        int j = 0;
+        if(n1 > n2) return findMedianSortedArrays(nums2, nums1);
+        int low = 0, high = n1;
+        int left = (n1 + n2 + 1) / 2;
         int n = n1 + n2;
-        int ind2 = n / 2;
-        int ind1 = ind2 - 1;
-        int ind1el = 0;
-        int ind2el = 0;
-        int count = 0;
-
-        while (i < n1 && j < n2 && count <= ind2)
+        while(low <= high)
         {
-            if (nums1[i] < nums2[j])
+            int mid1 = (low + high) / 2;
+            int mid2 = left - mid1;
+            int l1 = INT_MIN, l2 = INT_MIN;
+            int r1 = INT_MAX, r2 = INT_MAX;
+            if(mid1 < n1) r1 = nums1[mid1];
+            if(mid2 < n2) r2 = nums2[mid2];
+            if(mid1 - 1 >= 0) l1 = nums1[mid1 - 1];
+            if(mid2 - 1 >= 0) l2 = nums2[mid2 - 1];
+            if(l1 <= r2 && l2 <= r1)
             {
-                if (count == ind1) ind1el = nums1[i];
-                if (count == ind2) ind2el = nums1[i];
-                i++;
+                if(n % 2 == 1)  return max(l1, l2);
+                return (double)(max(l1, l2) + min(r1, r2)) / 2.0;
             }
-            else
-            {
-                if (count == ind1) ind1el = nums2[j];
-                if (count == ind2) ind2el = nums2[j];
-                j++;
-            }
-            count++;
+            else if(l1 > r2) high = mid1 - 1;
+            else low = mid1 + 1;
         }
-
-        while (i < n1 && count <= ind2)
-        {
-            if (count == ind1) ind1el = nums1[i];
-            if (count == ind2) ind2el = nums1[i];
-            i++;
-            count++;
-        }
-
-        while (j < n2 && count <= ind2)
-        {
-            if (count == ind1) ind1el = nums2[j];
-            if (count == ind2) ind2el = nums2[j];
-            j++;
-            count++;
-        }
-
-        if (n % 2 == 0)
-            return (double)(ind1el + ind2el) / 2.0;
-        else
-            return (double)ind2el;
+        return 0;
     }
 };
 
