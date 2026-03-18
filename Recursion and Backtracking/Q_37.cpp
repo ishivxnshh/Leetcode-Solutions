a16585faaf5c91dc5ad8eafc1a71a66d06a015c7
Shivansh Mittal <149358512+ishivxnshh@users.noreply.github.com>
2026-03-18 18:16:53 +0530

    Implement Sudoku solver using backtracking

---
+++ Recursion and Backtracking/Q_37.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isValid(vector<vector<char>>& board, int r, int col, char c)
+    {
+        for (int i = 0; i < 9; i++)
+        {
+            if (board[i][col] == c) return false;
+            if (board[r][i] == c) return false;
+            if (board[3 * (r / 3) + i / 3][3 * (col / 3) + i % 3] == c) return false;
+        }
+        return true;
+    }
+
+    bool solve(vector<vector<char>>& board) {
+        for (int i = 0; i < board.size(); i++)
+        {
+            for (int j = 0; j < board[0].size(); j++)
+            {
+                if (board[i][j] == '.') 
+                {
+                    for (char c = '1'; c <= '9'; c++)
+                    {
+                        if (isValid(board, i, j, c))
+                        {
+                            board[i][j] = c;
+
+                            if (solve(board)) return true;
+                            else board[i][j] = '.';
+                        }
+                    }
+                    return false;
+                }
+            }
+        }    
+        return true;
+    }
+
+    void solveSudoku(vector<vector<char>>& board) {
+        solve(board);
+    }
+};
+
+int main()
+{
+    // 🔹 Predefined Test Case
+    vector<vector<char>> board =
+    {
+        {'5','3','.','.','7','.','.','.','.'},
+        {'6','.','.','1','9','5','.','.','.'},
+        {'.','9','8','.','.','.','.','6','.'},
+        {'8','.','.','.','6','.','.','.','3'},
+        {'4','.','.','8','.','3','.','.','1'},
+        {'7','.','.','.','2','.','.','.','6'},
+        {'.','6','.','.','.','.','2','8','.'},
+        {'.','.','.','4','1','9','.','.','5'},
+        {'.','.','.','.','8','.','.','7','9'}
+    };
+
+    Solution obj;
+    obj.solveSudoku(board);
+
+    cout << "Solved Sudoku:\n";
+
+    for (int i = 0; i < 9; i++)
+    {
+        for (int j = 0; j < 9; j++)
+        {
+            cout << board[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
